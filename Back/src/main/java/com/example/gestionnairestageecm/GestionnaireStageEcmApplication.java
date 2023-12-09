d269f995c35d28f241fb80eff79aae2100ea0a69
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-12-09 22:44:58 +0100

    first commit

---
+++ Back/src/main/java/com/example/gestionnairestageecm/GestionnaireStageEcmApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionnairestageecm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireStageEcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionnaireStageEcmApplication.class, args);
+	}
+
+}
