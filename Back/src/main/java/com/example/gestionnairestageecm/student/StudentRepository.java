34a475364187433b111c0652ea6b6544ee26d7d8
ahmedidrissi <idrissiahmed2002@gmail.com>
2024-01-02 19:04:19 +0100

    create StudentRepository

---
+++ Back/src/main/java/com/example/gestionnairestageecm/student/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.gestionnairestageecm.student;
+
+// import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    // Optional<Student> findByEmail(String email);
+}
