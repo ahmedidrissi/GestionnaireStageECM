ca0f7babb0401702bf1383a61355f7d6509646b6
ahmedidrissi <idrissiahmed2002@gmail.com>
2024-01-02 19:03:59 +0100

    create Student class

---
+++ Back/src/main/java/com/example/gestionnairestageecm/student/Student.java
@@ -0,0 +1,42 @@
+package com.example.gestionnairestageecm.student;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "etudiant")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id", nullable = false)
+    private Long studentId;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+    
+    public Student(
+        String firstName,
+        String lastName,
+        String email
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    
+}
