9a893e80221ebadbff65a7c0044bf912ba461398
ahmedidrissi <idrissiahmed2002@gmail.com>
2024-01-04 20:27:08 +0100

    Refactor StudentController endpoints

--- Back/src/main/java/com/example/gestionnairestageecm/controllers/StudentController.java
+++ Back/src/main/java/com/example/gestionnairestageecm/controllers/StudentController.java
@@ -2,8 +2,6 @@ package com.example.gestionnairestageecm.controllers;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
-
 import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,51 +11,54 @@ import com.example.gestionnairestageecm.models.Student;
 import com.example.gestionnairestageecm.services.StudentService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 
 
-
-@Controller
+@RequestMapping("/students")
+@RestController
 @AllArgsConstructor
 public class StudentController {
     private StudentService studentService;
 
-    @GetMapping("/students")
+    @GetMapping("/list")
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
 
-    @GetMapping("/students/{studentId}")
+    @GetMapping("/id={studentId}")
     public Student getStudentById(@PathVariable Long studentId) {
         return studentService.getStudentById(studentId);
     }
 
-    // @GetMapping("/students/{email}")
-    // public Student getStudentByEmail(@PathVariable String email) {
-    //     return studentService.getStudentByEmail(email);
-    // }
+    @GetMapping("/email={email}")
+    public Student getStudentByEmail(@PathVariable String email) {
+        return studentService.getStudentByEmail(email);
+    }
 
-    // @GetMapping("/students/{firstName}/{lastName}")
-    // public Student getStudentByName(@PathVariable String firstName, @PathVariable String lastName) {
-    //     return studentService.getStudentByName(firstName, lastName);
-    // }
+    @GetMapping("/name={firstName}+{lastName}")
+    public Student getStudentByName(@PathVariable String firstName, @PathVariable String lastName) {
+        return studentService.getStudentByFirstNameAndLastName(firstName, lastName);
+    }
 
-    @PostMapping("/students/new")
+    @PostMapping("/new")
     public void saveStudent(@RequestBody Student student) {
         studentService.saveStudent(student);
     }
 
-    @PutMapping("/students/update/{studentId}")
-    public void updateStudent(@PathVariable String studentId, @RequestBody Student newStudent) {
-        studentService.updateStudent(newStudent);
+
+    @PutMapping("/update/id={studentId}")
+    public void updateStudent(@PathVariable Long studentId, @RequestBody Student newStudent) {
+        studentService.updateStudent(studentId, newStudent);
     }
 
-    @DeleteMapping("/students/delete/{studentId}")
+    @DeleteMapping("/delete/id={studentId}")
     public void deleteStudent(@PathVariable Long studentId) {
         studentService.deleteStudent(studentId);
-    }
+    } 
     
 }
