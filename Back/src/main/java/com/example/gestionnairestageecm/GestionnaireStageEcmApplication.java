d0d9fa59b943db411c17ed38accc4ff4a74f5a1d
ahmedidrissi <idrissiahmed2002@gmail.com>
2024-01-04 22:24:00 +0100

    delete run function

--- Back/src/main/java/com/example/gestionnairestageecm/GestionnaireStageEcmApplication.java
+++ Back/src/main/java/com/example/gestionnairestageecm/GestionnaireStageEcmApplication.java
@@ -1,13 +1,8 @@
 package com.example.gestionnairestageecm;
 
-// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
-// import com.example.gestionnairestageecm.models.Student;
-// import com.example.gestionnairestageecm.repositories.StudentRepository;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class GestionnaireStageEcmApplication {
@@ -15,25 +10,5 @@ public class GestionnaireStageEcmApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestionnaireStageEcmApplication.class, args);
 	}
-
-	// @Bean
-	// CommandLineRunner run(StudentRepository studentRepository) {
-	// 	return args -> {
-	// 		studentRepository.save(new Student(
-	// 			"John",
-	// 			"Doe",
-	// 			"john.doe@gmail.com",
-	// 			"Male",
-	// 			"01/01/2000",
-	// 			"1234 rue de la rue",
-	// 			"Montreal",
-	// 			"H1H 1H1",
-	// 			"514-123-4567",
-	// 			"2020",
-	// 			"01",
-	// 			"Passable"
-	// 		));
-	// 	};
-	// }
 
 }
