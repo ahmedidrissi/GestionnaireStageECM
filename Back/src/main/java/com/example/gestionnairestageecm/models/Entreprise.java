cc907ba8063534241a5ce37eabd8b75129f6c7c0
Emavero <diatemba11@gmail.com>
2024-01-04 19:51:34 +0100

    first commit

--- Back/src/main/java/com/example/gestionnairestageecm/models/Entreprise.java
+++ Back/src/main/java/com/example/gestionnairestageecm/models/Entreprise.java
@@ -3,9 +3,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 @Getter
@@ -19,47 +17,45 @@ public class Entreprise {
     @Column(name = "numero_siret", nullable = false)
     private Long numeroSiret;
 
-    @NotBlank
+    
     @Column(name = "forme_juridique", nullable = false)
     private String formeJuridique;
 
-    @NotBlank
+    
     @Column(name = "raison_sociale", nullable = false)
     private String raisonSociale;
 
-    @NotBlank
     @Column(name = "adresse", nullable = false)
     private String adresse;
 
-    @NotBlank
+    
     @Column(name = "ville", nullable = false)
     private String ville;
 
-    @NotNull
+
     @Column(name = "code_postal", nullable = false)
     private Integer codePostal;
 
     // Le téléphone du tuteur de l'étudiant dans l’entreprise
-    @NotBlank
+
     @Column(name = "telephone", nullable = false)
     private String telephone;
 
-    @NotBlank
+    
     @Column(name = "fax", nullable = false)
     private String fax;
 
     // Le numéro de téléphone de l’entreprise
-    @NotBlank
+    
     @Column(name = "contact", nullable = false)
     private String contact;
 
     // La personne joignable de l’entreprise
-    @NotBlank
+    
     @Column(name = "telephone_contact", nullable = false)
     private String telephoneContact;
 
-    @NotBlank
-    @Email
+   
     @Column(name = "email", nullable = false)
     private String email;
 
