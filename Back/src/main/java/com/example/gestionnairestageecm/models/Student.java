681a7e8f3f4266bd176ae3bfc42d604bfe2109e4
ahmedidrissi <idrissiahmed2002@gmail.com>
2024-01-04 19:05:06 +0100

    add more attributs

--- Back/src/main/java/com/example/gestionnairestageecm/models/Student.java
+++ Back/src/main/java/com/example/gestionnairestageecm/models/Student.java
@@ -20,23 +20,68 @@ public class Student {
     @Column(name = "student_id", nullable = false)
     private Long studentId;
 
-    @Column(name = "first_name", nullable = false)
+    @Column(name = "nom", nullable = false)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
+    @Column(name = "prenom", nullable = false)
     private String lastName;
 
     @Column(nullable = false)
     private String email;
+
+    @Column(name = "sexe", nullable = false)
+    private String gender;
+
+    @Column(name = "date_naissance", nullable = false)
+    private String dateOfBirth;
+
+    @Column(name = "adresse", nullable = false)
+    private String address;
+
+    @Column(name = "ville", nullable = false)
+    private String city;
+
+    @Column(name = "code_postal", nullable = false)
+    private String postalCode;
+
+    @Column(name = "telephone", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "promo", nullable = false)
+    private String promo;
+
+    @Column(name = "numero_promo", nullable = false)
+    private String promoNumber;
+
+    @Column(name = "mention", nullable = false)
+    private String mention;
     
     public Student(
         String firstName,
         String lastName,
-        String email
+        String email,
+        String gender,
+        String dateOfBirth,
+        String address,
+        String city,
+        String postalCode,
+        String phoneNumber,
+        String promo,
+        String promoNumber,
+        String mention
     ) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.phoneNumber = phoneNumber;
+        this.promo = promo;
+        this.promoNumber = promoNumber;
+        this.mention = mention;
     }
     
 }
