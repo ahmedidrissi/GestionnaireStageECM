6556e4ddd71358816d676897f4dae009c89c3196
ahmedidrissi <idrissiahmed2002@gmail.com>
2024-01-05 01:11:59 +0100

    Add annotations and columns to Promo model

--- Back/src/main/java/com/example/gestionnairestageecm/models/Promo.java
+++ Back/src/main/java/com/example/gestionnairestageecm/models/Promo.java
@@ -1,5 +1,7 @@
 package com.example.gestionnairestageecm.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +9,18 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity(name = "promos")
 public class Promo {
+
+    @Column(name = "annee", nullable = false)
     private int year;
+
+    @Column(name = "code_professeur", nullable = false)
     private long professorId;
+
+    @Column(name = "nombre_inscrits", nullable = false)
     private int registredNumber;
+
+    @Column(name = "nombre_reçus", nullable = false)
     private int receiptsNumber;
 }
