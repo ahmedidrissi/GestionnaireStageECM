e29681b87c5a58d9dbec91c1dce55f08d7841bc0
Emavero <diatemba11@gmail.com>
2024-01-06 23:50:09 +0100

    tutor

---
+++ Back/src/main/java/com/example/gestionnairestageecm/models/Tutor.java
@@ -0,0 +1,49 @@
+package com.example.gestionnairestageecm.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@AllArgsConstructor
+@Entity(name="tuteurs")
+public class Tutor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "numero_tuteur", nullable = false)
+    private Long tutorNumber;
+
+    @Column(name = "prenom", nullable = false)
+    private String firstName;
+
+    @Column(name = "nom", nullable = false)
+    private String lastName;
+
+    
+    @Column(name = "sexe", nullable = false)
+    private String gender;
+
+    @Column(name = "telephone_tuteur", nullable = false)
+    private String tutorPhoneNumber;
+
+public Tutor (
+    String firstName,
+    String lastName,
+    String gender,
+    String tutorPhoneNumber
+){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.gender=gender;
+    this.tutorPhoneNumber=tutorPhoneNumber;
+}
+    
+}
