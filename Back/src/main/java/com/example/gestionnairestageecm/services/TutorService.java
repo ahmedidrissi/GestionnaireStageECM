ae0dee333574337aece8e195d0f0408fe81b947d
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-01-09 21:30:16 +0100

    Add TutorService class

---
+++ Back/src/main/java/com/example/gestionnairestageecm/services/TutorService.java
@@ -0,0 +1,46 @@
+package com.example.gestionnairestageecm.services;
+
+import com.example.gestionnairestageecm.models.Tutor;
+import com.example.gestionnairestageecm.repositories.TutorRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class TutorService {
+
+    @Autowired
+    private TutorRepository tutorRepository; 
+    
+    public List<Tutor> getAllTutors() {
+        return tutorRepository.findAll();
+    }
+
+    public Tutor getTutorById(Long tutorNumber) {
+        return tutorRepository.findById(tutorNumber).get();
+    }
+
+    public Tutor getTutorByFirstNameAndLastName(String firstName, String lastName) {
+        return tutorRepository.findByFirstNameAndLastName(firstName, lastName).get();
+    }
+
+    public Tutor saveTutor(Tutor Tutor) {
+        return tutorRepository.save(Tutor);
+    }
+
+    public Tutor updateTutor(Long tutorNumber, Tutor newTutor) {
+        Tutor tutor = tutorRepository.findById(tutorNumber).get();
+        tutor.setFirstName(newTutor.getFirstName());
+        tutor.setLastName(newTutor.getLastName());
+        tutor.setGender(newTutor.getGender());
+        tutor.setTutorPhoneNumber(newTutor.getTutorPhoneNumber());
+        return tutorRepository.save(tutor);
+    }
+
+    public void deleteTutor(Long tutorNumber) {
+        tutorRepository.deleteById(tutorNumber);
+    }
+}
