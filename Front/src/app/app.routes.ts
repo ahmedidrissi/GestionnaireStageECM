b82b11c18b285c27b9d091809ca2567202e53bfd
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-01-09 23:01:18 +0100

    Add TutorsComponent to app.routes.ts

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AuthenticationComponent } from './components/authentication/authentication.component';
 import { HomeComponent } from './components/home/home.component';
 import { EntreprisesComponent } from './components/entreprises/entreprises.component';
+import { TutorsComponent } from './components/tutors/tutors.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'authenticate', component: AuthenticationComponent },
-    { path: 'entreprises', component: EntreprisesComponent }
+  { path: '', component: HomeComponent },
+  { path: 'authenticate', component: AuthenticationComponent },
+  { path: 'entreprises', component: EntreprisesComponent },
+  { path: 'tutors', component: TutorsComponent },
 ];
