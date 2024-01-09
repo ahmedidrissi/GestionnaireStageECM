6a3013c308296bd32ffc59c7797efd14fd24a7d5
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-01-09 02:19:02 +0100

    Add logout functionality and redirect to authentication page

--- Front/src/app/services/token-storage.service.ts
+++ Front/src/app/services/token-storage.service.ts
@@ -1,31 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenStorageService {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  clear(): void {
+  logout(): void {
     window.sessionStorage.clear();
+    this.router.navigate(['/authenticate']);
   }
 
-  public saveToken(token: string): void {
+  saveToken(token: string): void {
     window.sessionStorage.removeItem('token');
     window.sessionStorage.setItem('token', token);
   }
 
-  public getToken(): string | null {
+  getToken(): string | null {
     return window.sessionStorage.getItem('token');
   }
 
-  public saveUser(user: any): void {
+  saveUser(user: any): void {
     window.sessionStorage.removeItem('user');
     window.sessionStorage.setItem('user', JSON.stringify(user));
   }
 
-  public getUser(): any {
+  getUser(): any {
     const user = window.sessionStorage.getItem('user');
     if (user) {
       return JSON.parse(user);
